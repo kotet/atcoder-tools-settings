04903c6ffc61efb8f1b8eba08f91c684c1a7f75b
Kotet <kotet0418@gmail.com>
2021-10-03 19:08:16 +0900

    cpp

--- bashrc.sh
+++ bashrc.sh
@@ -42,3 +42,52 @@ atcsubmit() {
   atcoder-tools submit -u
   popd
 }
+
+
+cppinit() {
+  atcoder-tools gen --lang cpp --workspace ~/atcoder-workspace-cpp $1
+  cd ~/atcoder-workspace-cpp/$1
+}
+
+cpptest() {
+  (
+  pushd $1
+
+  local BUILD="g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -o a.out main.cpp"
+  local DEBUG="g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -o a.out main.cpp"
+
+  if [ "$2" = "debug" ]; then
+    echo $DEBUG
+    $DEBUG
+  else
+    echo $BUILD
+    $BUILD
+  fi
+
+  if [ ! $? = 0 ]; then
+    echo "Build failed."
+    return
+  fi
+
+  atcoder-tools test
+  popd
+  )
+}
+
+cppsubmit() {
+
+  pushd $1
+  
+  local BUILD="g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -o a.out main.cpp"
+
+  echo $BUILD
+  $BUILD
+
+  if [ ! $? = 0 ]; then
+    echo "Build failed."
+    return
+  fi
+
+  atcoder-tools submit -u
+  popd
+}
